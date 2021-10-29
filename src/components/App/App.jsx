b79c8959fd91bcab7cfebe55ef23b064a0c376bc
Matt Dow <matt.dow.vo@gmail.com>
2021-10-29 16:52:01 -0500

    Created redux store in index, starting routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { Route, HashRouter as Router, Link } from 'react-router-dom'
 
 function App() {
-
+  // render the components to the DOM as determined by Routes!
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+          
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+
+        <div className="bodyContent">
+          <Route path="/" exact>
+            
+
+          </Route>
+
+
+        </div>
+      </div>
+
+    </Router>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,53 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux'
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// What follows is a redux reducer to keep track of the current feedback 
+// being entered as the user proceeds through several form pages
+
+// Entering an example blank object for our initial state
+
+// const defaultFeedbackObject = {
+//     feeling: 0,
+//     understanding: 0,
+//     support: 0,
+//     comments: '',
+//     flagged: false,
+//     date: ''
+// }
+
+const currentFeedback = (state = {}, action) => {
+    if (action.type === 'ADD_FEELING') {
+        // adding a feeling key/value to the empty state object
+        return [...state, action.payload];
+    } else if (action.type === 'ADD_UNDERSTANDING') {
+        // adding an understanding key/value to current state object
+        return [...state, action.payload]
+    } else if (action.type === 'ADD_SUPPORT') {
+        // adding a support key/value to current state object
+        return [...state, action.payload]
+    } else if (action.type === 'ADD_COMMENTS') {
+        // adding a comments key/value to current state object
+        return [...state, action.payload]
+    }
+    // if action doesn't match any listed types, return the previous state
+    return state;
+ }
+
+// create a redux store
+const store = createStore(
+    combineReducers({
+        currentFeedback
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
 registerServiceWorker();
